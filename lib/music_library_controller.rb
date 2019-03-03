fb8a475dd7ebd068d50492a4f60f87c5a7a2323a
Joan Lyness <joanrigdon@gmail.com>
2019-03-03 04:25:26 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -67,11 +67,10 @@ class MusicLibraryController
     input = gets.strip
     sorted = Song.all.sort_by{|song| song.name}
     sorted.each_with_index do |song, index|
-      if input - 1 = index
+      if input - 1 == index
         puts "Playing #{song.name} by #{song.artist.name}."
       end
     end
   end
-
 
 end
